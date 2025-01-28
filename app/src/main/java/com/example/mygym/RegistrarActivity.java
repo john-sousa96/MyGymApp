e98888351f16b3fb47460793ef17d54afa1c7014
John Sousa <brenoloiola@gmail.com>
2025-01-27 21:03:16 -0300

    first commit

---
+++ app/src/main/java/com/example/mygym/RegistrarActivity.java
@@ -0,0 +1,15 @@
+package com.example.mygym;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+
+    }
+}
